fc0d1448c830af1c068de949d9eb988ea2f85729
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2021-02-14 18:23:03 +0300

    Работа над ошибками

--- Homework 2 - Three sliders/ViewController.swift
+++ Homework 2 - Three sliders/ViewController.swift
@@ -28,18 +28,44 @@ class ViewController: UIViewController {
         greenSlider.minimumTrackTintColor = .green
         
         colorChange()
+        
+        setValue(for: redSliderValue, greenSliderValue, blueSliderValue)
     }
     
-    @IBAction func slidersAction() {
-        redSliderValue.text = String(format:"%.2f", redSlider.value)
-        greenSliderValue.text = String(format:"%.2f", greenSlider.value)
-        blueSliderValue.text = String(format:"%.2f", blueSlider.value)
-        
+    
+    @IBAction func slidersAction(_ sender: UISlider) {
         colorChange()
+        
+        switch sender.tag {
+        case 0: redSliderValue.text = string(from: sender)
+        case 1: greenSliderValue.text = string(from: sender)
+        case 2: blueSliderValue.text = string(from: sender)
+        default: break
+        }
     }
     
     private func colorChange() {
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
     }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redSliderValue.text = string(from: redSlider)
+            case 1: greenSliderValue.text = string(from: greenSlider)
+            case 2: blueSliderValue.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
+   
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
 }
 
